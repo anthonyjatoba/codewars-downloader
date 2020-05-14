d96402d1139b1267fe8f84bf51fb6df93d306c4d
Anthony Jatobá <anthonyjatoba@gmail.com>
2020-05-14 16:57:50 -0300

    Implements writing solution and description to file

--- challenge.py
+++ challenge.py
@@ -1,9 +1,10 @@
 import reprlib
 
 class Challenge:
-    def __init__(self, id_, name, kyu, description):
+    def __init__(self, id_, name, url, kyu, description):
         self.id = id_
         self.name = name
+        self.url = url
         self.kyu = kyu
         self.description = description
         self.code = None
@@ -13,8 +14,14 @@ class Challenge:
         id_ = json['id']
         name = json['name']
         kyu = json['rank']['name']
-        description = json['description']
-        return cls(id_, name, kyu, description)
+        url = json['url']
+
+        description_header = '# [{}]({})'.format(name, url)
+        description_text = json['description'] 
+
+        description = '\n'.join([description_header, description_text])
+       
+        return cls(id_, name, url, kyu, description)
 
     def __str__(self):
         return '{} - {} - {}'.format(self.id, self.name, self.kyu)
--- codewars_downloader.py
+++ codewars_downloader.py
@@ -1,3 +1,4 @@
+import os
 import re
 import requests
 import json
@@ -15,6 +16,7 @@ if __name__ == '__main__':
         soup = BeautifulSoup(file.read(), 'lxml')
         solutions_elements = soup.findAll("div", {"class": "list-item solutions"})
 
+    challenges = {'{} kyu'.format(i):[] for i in range(1, 9)}
     for solution_element in solutions_elements[:3]:
         title = solution_element.find('div', {'class': 'item-title'})
         challenge_id = title.find('a')['href'].split('/')[-1]
@@ -22,10 +24,22 @@ if __name__ == '__main__':
         CHALLENGE_URL = CHALLENGE_BASE_URL.format(challenge_id)
         
         response = requests.get(CHALLENGE_URL)
-        js = response.json() 
+        js = response.json()
+        
         challenge = Challenge.fromjson(js)
 
         challenge.code = solution_element.findAll('code')[0].get_text()
-        print()
-        print(challenge)
-        print(challenge.code)
+        
+        challenges[challenge.kyu].append(challenge)
+
+    for key, challenge_list in challenges.items():
+        print(key)
+        for challenge in challenge_list:
+            dir = 'solutions/{}/{}'.format(key, challenge.name)
+            os.makedirs(dir, exist_ok=True)
+
+            with open(dir + '/' + 'README.md', 'w') as file:
+                file.write(challenge.description)
+
+            with open(dir + '/' + challenge.name + '.py', 'w') as file:
+                file.write(challenge.code)
